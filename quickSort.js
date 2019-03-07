700f8a58bbf034600713778a4402b7f91e66ceb6
Evan Guirino <etg624@gmail.com>
2019-03-07 09:16:31 -0800

    merge sort

--- quickSort.js
+++ quickSort.js
@@ -11,7 +11,7 @@ function swap(array, i, j) {
 
 function bubbleSort(array) {
   let swaps = 0;
-  for (let i=0; i<array.length - 1; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     if (array[i] > array[i + 1]) {
       swap(array, i, i + 1);
       swaps++;
@@ -28,24 +28,25 @@ function bubbleSort(array) {
 Quick Sort
 */
 
-const nums = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+const nums =
+  '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
 
 const numArray = nums.split(' ').map(num => Number(num));
 
 function partition(array, start, end) {
   const pivot = array[end - 1];
   let j = start;
-  for (let i=start; i<end - 1; i++) {
+  for (let i = start; i < end - 1; i++) {
     if (array[i] <= pivot) {
       swap(array, i, j);
       j++;
     }
   }
-  swap(array, end-1, j);
+  swap(array, end - 1, j);
   return j;
 }
 
-function qSort(arr, start=0, end=arr.length) {
+function qSort(arr, start = 0, end = arr.length) {
   // Base case
   if (start >= end) {
     return arr;
@@ -58,13 +59,9 @@ function qSort(arr, start=0, end=arr.length) {
   return arr;
 }
 
-qSort(numArray);
-console.log(JSON.stringify(numArray));
+// qSort(numArray);
+// console.log(JSON.stringify(numArray));
 
 /*
 Merge Sort
 */
-
-function mSort(arr) {
-  
-}
