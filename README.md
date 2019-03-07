061af6c7eabf9455ad2c1d764b36b925ed027e6c
John Young <john.young@spreadstreet.io>
2019-03-07 12:00:13 -0500

    Finished quick sort

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # DSA-Sorting-Evan-John
+
+## Quick Sort
+1. Choose a pivot (can be anything)
+2. Create two vars: i and j (set them to beginning)
+3. Compare i with pivot.
+4. If i is greater than pivot, move i forward 1
+5. If i is less than pivot, swap the values of i and j. then move j forward 1
+6. If we get to the end, we swap the pivot with j
+
+## Merge Sort
+1. Pick the middle
+2. Split left, and Split right until 1 element
+3. Start comparing splits based on index 0 of the arrays
+4. Recursively build new arrays from the mini sorted arrays
---
+++ quickSort.js
@@ -0,0 +1,70 @@
+/* eslint-disable strict */
+/* 
+Bubble Sort
+*/
+
+function swap(array, i, j) {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+}
+
+function bubbleSort(array) {
+  let swaps = 0;
+  for (let i=0; i<array.length - 1; i++) {
+    if (array[i] > array[i + 1]) {
+      swap(array, i, i + 1);
+      swaps++;
+    }
+  }
+
+  if (swaps > 0) {
+    return bubbleSort(array);
+  }
+  return array;
+}
+
+/* 
+Quick Sort
+*/
+
+const nums = '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
+
+const numArray = nums.split(' ').map(num => Number(num));
+
+function partition(array, start, end) {
+  const pivot = array[end - 1];
+  let j = start;
+  for (let i=start; i<end - 1; i++) {
+    if (array[i] <= pivot) {
+      swap(array, i, j);
+      j++;
+    }
+  }
+  swap(array, end-1, j);
+  return j;
+}
+
+function qSort(arr, start=0, end=arr.length) {
+  // Base case
+  if (start >= end) {
+    return arr;
+  }
+
+  const middle = partition(arr, start, end);
+  arr = qSort(arr, start, middle);
+  arr = qSort(arr, middle + 1, end);
+
+  return arr;
+}
+
+qSort(numArray);
+console.log(JSON.stringify(numArray));
+
+/*
+Merge Sort
+*/
+
+function mSort(arr) {
+  
+}
