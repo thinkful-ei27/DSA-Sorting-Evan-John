784f912f22d62e3be9d1021164f7047d365827ea
Evan Guirino <etg624@gmail.com>
2019-03-07 09:41:25 -0800

    counted operations

--- mergeSort.js
+++ mergeSort.js
@@ -1,7 +1,8 @@
-const nums =
-  '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
-const numArray = nums.split(' ').map(num => Number(num));
+const nums = '2 1 3';
+// '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
 
+const numArray = nums.split(' ').map(num => Number(num));
+let ticks = 0;
 function mSort(arr) {
   if (arr.length <= 1) {
     return arr;
@@ -14,6 +15,8 @@ function mSort(arr) {
   left = mSort(left);
   right = mSort(right);
 
+  ticks++;
+
   return merge(left, right, arr);
 }
 
@@ -22,6 +25,7 @@ function merge(left, right, array) {
   let rightIndex = 0;
   let outputIndex = 0;
   while (leftIndex < left.length && rightIndex < right.length) {
+    ticks++;
     if (left[leftIndex] < right[rightIndex]) {
       array[outputIndex++] = left[leftIndex++];
     } else {
@@ -30,13 +34,16 @@ function merge(left, right, array) {
   }
 
   for (let i = leftIndex; i < left.length; i++) {
+    ticks++;
     array[outputIndex++] = left[i];
   }
 
   for (let i = rightIndex; i < right.length; i++) {
+    ticks++;
     array[outputIndex++] = right[i];
   }
+
   return array;
 }
-
 console.log(JSON.stringify(mSort(numArray)));
+console.log(ticks);
--- quickSort.js
+++ quickSort.js
@@ -5,13 +5,16 @@ Bubble Sort
 
 function swap(array, i, j) {
   const tmp = array[i];
+  ticks++;
   array[i] = array[j];
+
   array[j] = tmp;
 }
 
 function bubbleSort(array) {
   let swaps = 0;
   for (let i = 0; i < array.length - 1; i++) {
+    ticks++;
     if (array[i] > array[i + 1]) {
       swap(array, i, i + 1);
       swaps++;
@@ -32,11 +35,12 @@ const nums =
   '89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5';
 
 const numArray = nums.split(' ').map(num => Number(num));
-
+let ticks = 0;
 function partition(array, start, end) {
   const pivot = array[end - 1];
   let j = start;
   for (let i = start; i < end - 1; i++) {
+    // ticks++;
     if (array[i] <= pivot) {
       swap(array, i, j);
       j++;
@@ -55,11 +59,12 @@ function qSort(arr, start = 0, end = arr.length) {
   const middle = partition(arr, start, end);
   arr = qSort(arr, start, middle);
   arr = qSort(arr, middle + 1, end);
-
+  // ticks++;
   return arr;
 }
 
-// qSort(numArray);
+bubbleSort(numArray);
+console.log(ticks);
 // console.log(JSON.stringify(numArray));
 
 /*
